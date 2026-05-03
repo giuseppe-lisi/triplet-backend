f8bb3ba6c13f7fd8f108188950bed7a73b1c6f0f
Giuseppe Lisi <giuseppe.lisi.main@gmail.com>
2026-05-03 18:36:55 +0200

    cancellazione a cascata delle canzoni associate ad album

--- src/main/java/com/mysongs/backend/model/Album.java
+++ src/main/java/com/mysongs/backend/model/Album.java
@@ -7,6 +7,7 @@ import org.springframework.format.annotation.DateTimeFormat;
 
 import com.fasterxml.jackson.annotation.JsonManagedReference;
 
+import jakarta.persistence.CascadeType;
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
@@ -46,7 +47,7 @@ public class Album {
     @DateTimeFormat(pattern = "yyyy-MM-dd")
     private LocalDate releaseDate;
 
-    @OneToMany(mappedBy = "album")
+    @OneToMany(mappedBy = "album", cascade = CascadeType.ALL)
     @JsonManagedReference
     private Set<Song> songs;
 
