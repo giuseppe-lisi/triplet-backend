4b5d9956c29f4024de97ea49d84cb46118999ea6
Giuseppe Lisi <giuseppe.lisi.main@gmail.com>
2026-05-03 22:37:09 +0200

    added custom query

--- src/main/java/com/mysongs/backend/repositories/AlbumsRepository.java
+++ src/main/java/com/mysongs/backend/repositories/AlbumsRepository.java
@@ -1,5 +1,7 @@
 package com.mysongs.backend.repositories;
 
+import java.util.List;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
@@ -7,5 +9,5 @@ import com.mysongs.backend.model.Album;
 
 @Repository
 public interface AlbumsRepository extends JpaRepository<Album, Integer> {
-    // crud methods go here
+    public List<Album> findByTitleContaining(String title);
 }
