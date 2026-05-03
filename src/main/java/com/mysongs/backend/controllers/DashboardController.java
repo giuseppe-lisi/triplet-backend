7bd74cdb3e07e94f6863fe193d5b6eade843c9ed
Giuseppe Lisi <giuseppe.lisi.main@gmail.com>
2026-05-03 22:36:27 +0200

    added dashboard info

--- src/main/java/com/mysongs/backend/controllers/DashboardController.java
+++ src/main/java/com/mysongs/backend/controllers/DashboardController.java
@@ -1,16 +1,30 @@
 package com.mysongs.backend.controllers;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
+import com.mysongs.backend.service.DashboardService;
+
 @Controller
 @RequestMapping("/")
 public class DashboardController {
 
+    @Autowired
+    private DashboardService dashboardService;
+
     @GetMapping
     public String dashboard(Model model) {
+        Integer genres = dashboardService.getNumOfGenres();
+        Integer songs = dashboardService.getNumOfSongs();
+        Integer albums = dashboardService.getNumOfAlbums();
+
+        model.addAttribute("genres", genres);
+        model.addAttribute("songs", songs);
+        model.addAttribute("albums", albums);
+
         return "dashboard";
     }
     
