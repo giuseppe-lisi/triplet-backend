7f02fd9a7a072efddae8bdce2b1868b424c05cec
Giuseppe Lisi <giuseppe.lisi.main@gmail.com>
2026-05-04 16:02:32 +0200

    controller rest for songs and albums entities, tested via postman

--- src/main/java/com/triplet/backend/restControllers/AlbumsRestController.java
+++ src/main/java/com/triplet/backend/restControllers/AlbumsRestController.java
@@ -1,15 +1,21 @@
 package com.triplet.backend.restControllers;
 
 import java.util.List;
+import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.triplet.backend.model.Album;
 import com.triplet.backend.service.AlbumsService;
 
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
 
 @RestController
 @RequestMapping("/api/albums")
@@ -21,6 +27,17 @@ public class AlbumsRestController {
     @GetMapping
     public List<Album> index() {
         return albumsService.getAllAlbums();
+    }
+    
+    @GetMapping("/{id}")
+    public ResponseEntity<Album> show(@PathVariable Integer id) {
+        Optional<Album> album = albumsService.getAlbumById(id);
+
+        if (album.isEmpty()) {
+            return new ResponseEntity<Album>(HttpStatus.NOT_FOUND);
+        }
+        
+        return new ResponseEntity<Album>(album.get(), HttpStatus.OK);
     }
     
 }
---
+++ src/main/java/com/triplet/backend/restControllers/SongsRestController.java
@@ -0,0 +1,40 @@
+package com.triplet.backend.restControllers;
+
+import java.util.List;
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.triplet.backend.model.Song;
+import com.triplet.backend.repositories.SongsRepository;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@RestController
+@RequestMapping("/api/songs")
+public class SongsRestController {
+    
+    @Autowired
+    private SongsRepository songsRepository;
+
+    @GetMapping
+    public List<Song> index() {
+        return songsRepository.findAll();
+    }
+    
+    @GetMapping("/{id}")
+    public ResponseEntity<Song> show(@PathVariable Integer id) {
+        Optional<Song> song = songsRepository.findById(id);
+
+        if (song.isEmpty()) {
+            return new ResponseEntity<Song>(HttpStatus.NOT_FOUND);
+        }
+        
+        return new ResponseEntity<Song>(song.get(), HttpStatus.OK);
+    }
+}
